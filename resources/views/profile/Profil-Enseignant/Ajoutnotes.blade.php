8ea36ab8d6a2f7b2b9f59103216c513b0386fa65
SagoutiHicham <90241079+SagoutiHicham@users.noreply.github.com>
2023-06-20 11:43:20 +0200

    la recuperation de la partie d'abdou sur la nV-cversion

--- resources/views/profile/Profil-Enseignant/Ajoutnotes.blade.php
+++ resources/views/profile/Profil-Enseignant/Ajoutnotes.blade.php
@@ -1,67 +1,117 @@
-
 @extends('layouts.model')
 @section('title','Offre d\'alternance')
 @section('content')
 
 
 
-    <ul class="listLien">
-        <li>ACCUEIL </li>
-        <li> > </li>
-        <li>ESPACE ENSEIGNANT</li>
-        <li> > </li>
-        <li>NOTES</li>
-    </ul>
-
-
-    <section id="section">
-
-    @include('layouts.sidebarProf')
-
-
-        <div class="Ajoutnotes mx-auto my-7">
-    <h2>Ajouter des notes</h2>
-
-    <form action="traitement_formulaire.php" method="POST">
-
-    <div>
-   <ul class="flex flex-row justify-center">
-    <li> <input type="radio" name="classe" id="classe1" value="classe1" required>
-  <label for="classe1">LP MIE</label></li>
-  <br>
-  <li> <input type="radio" name="classe" id="classe2" value="classe2">
-  <label for="classe2">LP MIEF</label></li>
-  <br>
-  <li> <input type="radio" name="classe" id="classe3" value="classe3">
-  <label for="classe3">LP MRI</label></li>
+<ul class="listLien">
+  <li>ACCUEIL </li>
+  <li> > </li>
+  <li>ESPACE ENSEIGNANT</li>
+  <li> > </li>
+  <li>NOTES</li>
 </ul>
+
+
+<section id="section">
+
+  @include('layouts.sidebarProf')
+
+
+  <div class="Ajoutnotes mx-auto my-7">
+    <h2>Ajouter des notes</h2>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+    @endif
+    <form action="{{ route('Ajoutnotes.add') }}" method="POST">
+      @csrf
+      <div>
+        <ul class="flex flex-row justify-center">
+          <li> <input type="radio" name="classe" class="choixClasse" id="classe1" value="1" required>
+            <label for="classe1">LP MIE</label>
+          </li>
+          <br>
+          <li> <input type="radio" name="classe" class="choixClasse" id="classe2" value="2">
+            <label for="classe2">LP MIEF</label>
+          </li>
+          <br>
+          <li> <input type="radio" name="classe" class="choixClasse" id="classe3" value="3">
+            <label for="classe3">LP MRI</label>
+          </li>
+        </ul>
+      </div>
+
+      <br><br>
+      <input type="hidden" name="id_enseignant" value="{{auth()->id();}}">
+
+      <select name="id_etudiant" id="etudiant" required>
+        <option value="" selected>Etudiant</option>
+      </select>
+      <br><br>
+      <select name="id_matiere" id="matiere" required>
+        <option value="" selected>Matière</option>
+        @foreach ($matieres as $matiere)
+        <option value="{{$matiere->id_matiere}}">{{$matiere->nom_matiere}}</option>
+        @endforeach
+      </select> <br><br>
+
+      <input type="number" name="note" id="note" placeholder="Note*" min="0" max="20" step="0.5" required>
+      <br><br>
+
+      <input type="number" name="coefficient" id="coefficient" placeholder="Coefficient*" min="1" step="1" required>
+      <br><br>
+
+      <textarea name="appreciation" id="appreciation" placeholder="Appréciation*" rows="4" cols="30" required></textarea>
+      <br><br>
+
+      <input type="submit" value="Ajouter la note">
+    </form>
   </div>
+</section>
 
-  <br><br>
-  
-  <input type="text" name="nom" id="nom" placeholder="Nom*" required>
-  <br><br>
+<script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous">
+</script>
+<script>
+  jQuery(document).ready(function() {
+    jQuery('.choixClasse').click(function(e) {
+      var $idClasseChoisie = $(this).val();
 
-  <input type="text" name="prenom" id="prenom" placeholder="Prénom*" required>
-  <br><br>
+      //  e.preventDefault();
+      $.ajaxSetup({
+        headers: {
+          'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+        }
+      });
+      jQuery.ajax({
+        url: "/Ajoutnotes/showClasse" + $idClasseChoisie,
+        method: 'get',
+        success: function(result) {
+          var optionDeBase = $('<option></option>').attr("value", "").text("Etudiant");
+          $("#etudiant").empty().append(optionDeBase);
 
-  <input type="text" name="matiere" id="matiere" placeholder="Matière*" required>
-  <br><br>
+          // Les étudiants
+          $.each(result, function(index, element) {
+            var nomEtudiant = element.nom_etudiant;
+            var idEtudiant = element.id_etudiant;
+            var optionEtudiant = $('<option></option>').attr("value", idEtudiant).text(nomEtudiant);
+            $("#etudiant").append(optionEtudiant);
+          });
 
-  <input type="number" name="note" id="note" placeholder="Note*" min="0" max="20" step="0.5" required>
-  <br><br>
 
-  <input type="number" name="coefficient" id="coefficient" placeholder="Coefficient*" min="1" step="1" required>
-  <br><br>
 
-  <textarea name="appreciation" id="appreciation" placeholder="Appréciation*" rows="4" cols="30" required></textarea>
-  <br><br>
 
-  <input type="submit" value="Ajouter la note">
-</form>
-</div>
 
-    </section>
+        }
+      });
+    });
+  });
+</script>
 
 
 
@@ -70,8 +120,8 @@
 
 
 <style>
-/*FORM*/ 
-    .Ajoutnotes {
+  /*FORM*/
+  .Ajoutnotes {
     background-color: #f2f2f2;
     padding: 20px;
     border-radius: 5px;
@@ -115,64 +165,65 @@
   .Ajoutnotes input[type="submit"]:hover {
     background-color: #BC6C25;
   }
-/*FIN FORM*/ 
 
-#section {
-        min-height: 30vw;
-        height: 100%;
-        DISPLAY: flex;
+  /*FIN FORM*/
 
-
-    }
-
-    ul.listLien {
-        display: flex;
-        padding: 18px;
-        color: white;
-        font-size: 13px;
-        border-bottom: 1px solid;
-        background: #606c38;
-    }
-
-    ul.listLien li {
-        padding-left: 11px;
-        position: relative;
-        left: 7pc;
-    }
-
-    .lienFormation {
-        background-color: #283618;
-        padding: 19px;
+  #section {
+    min-height: 30vw;
+    height: 100%;
+    DISPLAY: flex;
 
 
-    }
+  }
 
- 
-    .lienFormation h2 {
-        color: white;
-        position: relative;
-        margin: auto 114px;
-    }
+  ul.listLien {
+    display: flex;
+    padding: 18px;
+    color: white;
+    font-size: 13px;
+    border-bottom: 1px solid;
+    background: #606c38;
+  }
 
-    .listMenu {
+  ul.listLien li {
+    padding-left: 11px;
+    position: relative;
+    left: 7pc;
+  }
 
-        width: 18%;
-        text-align: left;
-        /* height: 100%; */
-        background: #606c38;
-        color: white;
-
-
-    }
-
-    .listMenu li {
-        border-bottom: 1px solid white;
-        padding: 15px;
-        text-align: center;
-
-    }
+  .lienFormation {
+    background-color: #283618;
+    padding: 19px;
 
 
-    div.menu {}
+  }
+
+
+  .lienFormation h2 {
+    color: white;
+    position: relative;
+    margin: auto 114px;
+  }
+
+  .listMenu {
+
+    width: 18%;
+    text-align: left;
+    /* height: 100%; */
+    background: #606c38;
+    color: white;
+
+
+  }
+
+  .listMenu li {
+    border-bottom: 1px solid white;
+    padding: 15px;
+    text-align: center;
+
+  }
+
+
+  div.menu {}
 </style>
 @endsection
